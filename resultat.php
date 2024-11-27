0e5fcbfb79610f264171b690ad8d4f3ba359aa34
Ross260 <rossketcha@gmail.com>
2024-11-27 10:20:42 +0100

    results display

--- resultat.php
+++ resultat.php
@@ -1,10 +1,38 @@
 <?php
 
+include "connect.php";
+
 var_dump($_POST);
 
-//question 12 tu as répondu rep 49
+$score = 0;
 foreach ($_POST as $key => $value) {
-    # code...
+    // echo "A la question $key tu as repondu $value <br>";
+
+    $sql = "select * from reponses where idr = $value";
+    $resultat = mysqli_query($id, $sql);
+    $ligne = mysqli_fetch_assoc($resultat);
+    if($ligne["verite"] == 1) {
+        $score = $score + 2;
+    }else {
+        $sql2 = "select * from questions where idq = $key";
+        $resultat2 = mysqli_query($id, $sql2);
+        $ligne2 = mysqli_fetch_assoc($resultat2);
+        echo "Erreur à la question : ".$ligne2["libelleQ"]."<br>";
+        $sql2 = "select * from reponses where idq = $key and verite = 1";
+        $resultat2 = mysqli_query($id, $sql2);
+        $ligne2 = mysqli_fetch_assoc($resultat2);
+        echo "La bonne reponse était : ".$ligne2["libeller"]."br";
+
+
+    }
+
+
 }
+echo "<br> $score / 20";
 
 ?>
+
+<!-- 
+
+
+-->
