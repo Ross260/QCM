962f6af42e6af826581324206f4e8ae63f066985
Ross260 <rossketcha@gmail.com>
2024-11-27 09:34:36 +0100

    send form to result page

--- listeQuestions.php
+++ listeQuestions.php
@@ -8,28 +8,31 @@
 <body>
     <h1>Tentez votre chance </h1><hr>
 
-    <?php
-    include "./connect.php";
-    $sql = "SELECT * FROM questions order by rand() limit 10";
-    $resultat = mysqli_query($id, $sql);
+    <form action="resultat.php" method="post">
+        <?php
+            include "./connect.php";
+            $sql = "SELECT * FROM questions order by rand() limit 10";
+            $resultat = mysqli_query($id, $sql);
 
-    //boucle d'affichage des questions
-    $i = 1;
-    while ($ligne = mysqli_fetch_assoc($resultat)) {
-        echo "<h3> $i : ".$ligne['libelleQ']."<br>";
-        $i++;
+            //boucle d'affichage des questions
+            $i = 1;
+            while ($ligne = mysqli_fetch_assoc($resultat)) {
+                echo "<h3> $i : ".$ligne['libelleQ']."<br>";
+                $i++;
 
-        $sql2 = "select * from reponses where idq = ".$ligne["idq"];
-        $resultat2 = mysqli_query($id, $sql2);
-        while ($ligne2 = mysqli_fetch_assoc($resultat2)) {
-            echo $ligne2["libeller"]."<br>";
-        }
+                $sql2 = "select * from reponses where idq = ".$ligne["idq"];
+                $resultat2 = mysqli_query($id, $sql2);
+                while ($ligne2 = mysqli_fetch_assoc($resultat2)) {
+                    $idq = $ligne2['idq'];
+                    $idr = $ligne2['idr'];
+                    echo "<input type='radio' name='$idq' value='$idr' checked> ".$ligne2["libeller"]."<br>";
+                }
 
+            }
 
-
-    }
-
-    ?>
-
+        ?>
+        <hr>
+        <input type="submit" value="ENVOYER">
+    </form>
 </body>
 </html>
---
+++ resultat.php
@@ -0,0 +1,10 @@
+<?php
+
+var_dump($_POST);
+
+//question 12 tu as répondu rep 49
+foreach ($_POST as $key => $value) {
+    # code...
+}
+
+?>
